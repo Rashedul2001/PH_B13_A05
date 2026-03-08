c9465d3a0a63a31b582cda9f21fdc79e9f933a3b
Rashedul2001 <hacker360sr@gmail.com>
2026-03-08 12:18:47 +0600

    Fix path issue for home page

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -4,7 +4,7 @@ const handleLogIn = () => {
     const userName = document.getElementById("user-name");
     const password = document.getElementById("password");
     if (userName.value === "admin" && password.value === "admin123") {
-        window.location.replace("/pages/home.html");
+        window.location.replace("./pages/home.html");
     }
     else {
         alert("Invalid username or password");

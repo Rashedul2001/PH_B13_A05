20114572e1e1168233290448ba675833045d622f
Rashedul2001 <hacker360sr@gmail.com>
2026-03-08 11:49:45 +0600

    implement login functionality

---
+++ scripts/mainScript.js
@@ -0,0 +1,16 @@
+console.log("script is running");
+
+const handleLogIn = () => {
+    const userName = document.getElementById("user-name");
+    const password = document.getElementById("password");
+    if (userName.value === "admin" && password.value === "admin123") {
+        window.location.replace("/pages/home.html");
+    }
+    else {
+        alert("Invalid username or password");
+        userName.value = "";
+        password.value = "";
+    }
+}
+document.getElementById("submit-btn").addEventListener("click", handleLogIn);
+
